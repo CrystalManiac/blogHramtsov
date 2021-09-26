efcfa91f129fb1a28b7c5a2e7cd60c83aabc77b9
dhramtsov <dhramtsov@digital-spirit.ru>
2021-09-26 18:06:19 +0400

    Добавьте файлы проекта.

---
+++ blogHramtsov/Controllers/BlogController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace blogHramtsov.Controllers
+{
+    // Контроллер страницы блога
+    public class BlogController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ blogHramtsov/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using blogHramtsov.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace blogHramtsov.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        // Метод отображения представления главной страницы
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // Метод  отображения представления страницы ПК
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+
+        // Обработка ошибок отображения представлений
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
